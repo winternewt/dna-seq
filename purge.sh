e7ee26119d9d62b73df232aaff7b2f0f9e62ca40
Newton Winter <isoutthere@gmail.com>
2021-01-23 06:01:53 -0800

    dna-tools

---
+++ purge.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if ! [ $(id -u) = 0 ]; then
+   echo "Must be root!"
+   exit 1
+fi
+docker system prune -a
+rm -rf /data/cromwell-executions/
+rm -rf /data/cromwell-workflow-logs/workflow.*
+rm -rf /data/databases/mysql/*
